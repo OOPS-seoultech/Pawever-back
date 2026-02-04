ea3ea9651a7ae2ef3874ff868e3b185d373da370
Hyo <lovellehyo@gmail.com>
2026-02-04 11:10:19 +0900

    spring boot application 초기화

---
+++ src/main/java/com/pawever/backend/PaweverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pawever.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaweverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaweverBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawever/backend/PaweverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawever.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaweverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
