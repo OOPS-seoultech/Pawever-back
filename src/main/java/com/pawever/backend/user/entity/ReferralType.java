059e0debd9a7f24a54342fc2a3486262894ef6c3
Hyo <lovellehyo@gmail.com>
2026-02-23 08:43:38 +0900

    가입경로 enum 처리

--- src/main/java/com/pawever/backend/user/entity/ReferralType.java
+++ src/main/java/com/pawever/backend/user/entity/ReferralType.java
@@ -1,5 +1,9 @@
 package com.pawever.backend.user.entity;
 
 public enum ReferralType {
-    INSTAGRAM, YOUTUBE, FRIEND, OTHER
+    FRIEND,     // 주변에서 추천해서
+    THREADS,    // 쓰레드 보고
+    INSTAGRAM,  // 인스타그램 보고
+    OFFLINE,    // 동물병원·장례식장 등 오프라인 소개
+    OTHER       // 기타
 }
