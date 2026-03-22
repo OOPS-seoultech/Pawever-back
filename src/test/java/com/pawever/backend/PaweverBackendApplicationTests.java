fdfb2ec7c83dd86f3292392c2a6bd374e7eb64ae
prislewarz <whdwns5060@naver.com>
2026-03-22 21:05:49 +0900

    test: 통합 테스트 환경 구축 및 설정 분리

--- src/test/java/com/pawever/backend/PaweverBackendApplicationTests.java
+++ src/test/java/com/pawever/backend/PaweverBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.pawever.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaweverBackendApplicationTests {
 
 	@Test
